1529006e65733071cd3cb97dfd6f8b0c2401c3b8
Anand Kumar <kumara0398@gmail.com>
2019-11-12 18:12:37 -0500

    cherry pick changes to main

--- insn/main.sml
+++ insn/main.sml
@@ -1,36 +1,30 @@
 structure Main = struct
+  fun getsome (SOME x) = x
 
-   structure Tr = Translate
-   (* structure F = Frame *)
-   (* structure R = RegAlloc *)
+  fun emitproc out (Frame.PROC{body,frame}) =
+    let val _ = print ("emit " ^ Symbol.name(Frame.name(frame)) ^ "\n")
+        (*         val _ = Printtree.printtree(out,body); *)
+        val stms = Canon.linearize body
+        (*         val _ = app (fn s => Printtree.printtree(out,s)) stms; *)
+        val stms' = Canon.traceSchedule(Canon.basicBlocks stms)
+        val instrs = List.concat(map (MipsGen.codegen frame) stms')
+        val format0 = Assem.format(Temp.makestring)
+    in
+      app (fn i => TextIO.output(out, format0 i)) instrs
+    end
+    | emitproc out (F.STRING(lab,s)) = TextIO.output(out, s)
 
- fun getsome (SOME x) = x
+  fun withOpenFile fname f =
+    let val out = TextIO.openOut fname
+    in (f out before TextIO.closeOut out)
+      handle e => (TextIO.closeOut out; raise e)
+    end
 
-   fun emitproc out (F.PROC{body,frame}) =
-     let
-         (* val _ = print ("emit " ^ Frame.name frame ^ "\n") *)
-(*         val _ = Printtree.printtree(out,body); *)
-	       val stms = Canon.linearize body
-(*         val _ = app (fn s => Printtree.printtree(out,s)) stms; *)
-         val stms' = Canon.traceSchedule(Canon.basicBlocks stms)
-	       val instrs =   List.concat(map (MipsGen.codegen frame) stms')
-         val format0 = Assem.format(Temp.makestring)
-      in  app (fn i => TextIO.output(out,format0 i)) instrs
-      end
-    | emitproc out (F.STRING(lab,s)) = ()
-
-   fun withOpenFile fname f =
-       let val out = TextIO.openOut fname
-        in (f out before TextIO.closeOut out)
-	    handle e => (TextIO.closeOut out; raise e)
-       end
-
-   fun compile filename =
-       let val absyn = Parse.parse filename
-           val frags = (FindEscape.findEscape absyn; Semant.transProg absyn)
-        in
-            withOpenFile (filename ^ ".s")
-	     (fn out => (app (emitproc out) frags))
-       end
-
+  fun compile filename =
+    let val absyn = Parse.parse filename
+        val frags = (FindEscape.findEscape absyn; Semant.transProg absyn)
+    in
+      withOpenFile (filename ^ ".s")
+      (fn out => (app (emitproc out) frags))
+    end
 end
--- tests/queens.tig.s
+++ tests/queens.tig.s
@@ -1,129 +1,132 @@
-L50:
+L144:
 lw 'd0, 4('s0)
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-beq t277, t278, L46
-L47:
+beq t831, t832, L140
+L141:
 li 'd0, 0move 'd0, 's0
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1ble t154, t281, L44
-L10:
+addi 'd0, 's0, ~1ble t708, t835, L138
+L104:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L46:
-la t285, L15
+L140:
+la t839, L109
 lw 'd0, 0('s0)move 'd0, 's0
-jal t285
-move t171, t103
+jal t839
+move t725, t103
 j 'j0
-L44:
+L138:
 lw 'd0, 0('s0)lw 'd0, ~8('s0)
 li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-lw 'd0, 0('s0)beqz t292, L31
-L32:
+lw 'd0, 0('s0)beqz t846, L125
+L126:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L31:
+L125:
 li 'd0, 1move 'd0, 's0
 lw 'd0, 0('s0)lw 'd0, ~16('s0)
 lw 'd0, 4('s0)
 add 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-lw 'd0, 0('s0)beqz t302, L34
-L35:
+lw 'd0, 0('s0)beqz t856, L128
+L129:
 li 'd0, 0move 'd0, 's0
-L34:
+L128:
 j 'j0
-L51:
-li 'd0, 1beq t304, t157, L36
-L37:
+L145:
+li 'd0, 1beq t858, t711, L130
+L131:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L36:
+L130:
 li 'd0, 1move 'd0, 's0
 lw 'd0, 0('s0)lw 'd0, ~20('s0)
 addi 'd0, 's0, 7lw 'd0, 4('s0)
 sub 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-lw 'd0, 0('s0)beqz t315, L39
-L40:
+lw 'd0, 0('s0)beqz t869, L133
+L134:
 li 'd0, 0move 'd0, 's0
-L39:
+L133:
 j 'j0
-L52:
-li 'd0, 1beq t317, t160, L41
-L42:
+L146:
+li 'd0, 1beq t871, t714, L135
+L136:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L41:
+L135:
 lw 'd0, 0('s0)lw 'd0, ~20('s0)
 addi 'd0, 's0, 7lw 'd0, 4('s0)
 sub 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 0sw t327, (t168)
+li 'd0, 0sw t881, (t722)
 lw 'd0, 0('s0)lw 'd0, ~16('s0)
 lw 'd0, 4('s0)
 add 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 0sw t335, (t167)
+li 'd0, 0sw t889, (t721)
 lw 'd0, 0('s0)lw 'd0, ~8('s0)
 li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 0sw t341, (t166)
-la t342, L29
+li 'd0, 0sw t895, (t720)
+la t896, L123
 lw 'd0, 0('s0)move 'd0, 's0
 lw 'd0, 4('s0)
 addi 'd0, 's0, 1move 'd0, 's0
-jal t342
-move t276, t103
+jal t896
+move t830, t103
 lw 'd0, 0('s0)lw 'd0, ~12('s0)
 lw 'd0, 4('s0)
 li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-sw t154, (t165)
+sw t708, (t719)
 lw 'd0, 0('s0)lw 'd0, ~20('s0)
 addi 'd0, 's0, 7lw 'd0, 4('s0)
 sub 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 1sw t360, (t164)
+li 'd0, 1sw t914, (t718)
 lw 'd0, 0('s0)lw 'd0, ~16('s0)
 lw 'd0, 4('s0)
 add 'd0, 's0, 's1li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 1sw t368, (t163)
+li 'd0, 1sw t922, (t717)
 lw 'd0, 0('s0)lw 'd0, ~8('s0)
 li 'd0, 4mul 'd0, 's0, 's1add 'd0, 's0, 's1move 'd0, 's0
-li 'd0, 1sw t374, (t162)
+li 'd0, 1sw t928, (t716)
 li 'd0, 0move 'd0, 's0
 j 'j0
-L53:
+L147:
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1beq t154, t376, L10
-L45:
+addi 'd0, 's0, ~1beq t708, t930, L104
+L139:
 addi 'd0, 's0, 1move 'd0, 's0
 j 'j0
-L54:
+L148:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L49:
-L56:
+L143:
+L150:
 li 'd0, 0sw 's1, ~4('s0)
 lw 'd0, ~4('s0)
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1ble t382, t383, L26
-L10:
+addi 'd0, 's0, ~1ble t936, t937, L120
+L104:
 li 'd0, 0move 'd0, 's0
 j 'j0
-L26:
+L120:
 li 'd0, 0sw 's1, ~8('s0)
 lw 'd0, ~8('s0)
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1ble t388, t389, L23
-L16:
+addi 'd0, 's0, ~1ble t942, t943, L117
+L110:
 lw 'd0, ~4('s0)
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1beq t392, t393, L10
-L27:
+addi 'd0, 's0, ~1beq t946, t947, L104
+L121:
 lw 'd0, ~4('s0)
 addi 'd0, 's0, 1sw 's1, ~4('s0)
 j 'j0
-L23:
+L117:
 lw 'd0, ~8('s0)
 lw 'd0, 0('s0)lw 'd0, ~4('s0)
-addi 'd0, 's0, ~1beq t398, t399, L16
-L24:
+addi 'd0, 's0, ~1beq t952, t953, L110
+L118:
 lw 'd0, ~8('s0)
 addi 'd0, 's0, 1sw 's1, ~8('s0)
 j 'j0
-L55:
+L149:
+
+
+ . O
