7ff592f2703e2a2995e2de56e7426d788004eb61
David Reed <dreed@drift.com>
2019-11-12 19:01:56 -0500

    procEntryExit on main, modified tiger insns

--- tests_insn/factorial.tig.s
+++ tests_insn/factorial.tig.s
@@ -1,18 +1,28 @@
 L18:
+la t145, L13
+li 'd0, 8
+move 'd0, 's0
+jal t145
+move t144, t103
+li 'd0, 0
+move 'd0, 's0
+j 'j0
+L17:
+L20:
 lw 'd0, 4('s0)
 li 'd0, 1
-ble t141, t142, L14
+ble t150, t151, L14
 L15:
 lw 'd0, 4('s0)
 move 'd0, 's0
-la t144, L13
+la t153, L13
 lw 'd0, 0('s0)
 move 'd0, 's0
 lw 'd0, 4('s0)
 addi 'd0, 's0, ~1
 move 'd0, 's0
-jal t144
-move t139, t103
+jal t153
+move t148, t103
 mul 'd0, 's0, 's1
 move 'd0, 's0
 j 'j0
@@ -20,8 +30,8 @@ L14:
 li 'd0, 1
 move 'd0, 's0
 j 'j0
-L19:
+L21:
 li 'd0, 0
 move 'd0, 's0
 j 'j0
-L17:
+L19:
