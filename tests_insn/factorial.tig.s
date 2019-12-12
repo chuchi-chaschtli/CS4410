406c973ddaa363f3b0967169e6c5d7abee8b9630
David James Reed <ddavidreed@gmail.com>
2019-12-12 02:14:01 -0500

    Real assembly

--- tests_insn/factorial.tig.s
+++ tests_insn/factorial.tig.s
@@ -1,37 +1,59 @@
-L18:
-la t145, L13
-li `d0, 8
-move `d0, `s0
-jal t145
-move t144, t103
-li `d0, 0
-move `d0, `s0
-j `j0
+	.globl main
+	.data
+
+	.text
+main:
+sw $fp 0($sp)
+move $fp $sp
+addi $sp $sp -20
 L17:
-L20:
-lw `d0, 4(`s0)
-li `d0, 1
-ble t150, t151, L14
-L15:
-lw `d0, 4(`s0)
-move `d0, `s0
-la t153, L13
-lw `d0, 0(`s0)
-move `d0, `s0
-lw `d0, 4(`s0)
-addi `d0, `s0, ~1
-move `d0, `s0
-jal t153
-move t148, t103
-mul `d0, `s0, `s1
-move `d0, `s0
-j `j0
-L14:
-li `d0, 1
-move `d0, `s0
-j `j0
-L21:
-li `d0, 0
-move `d0, `s0
-j `j0
+lw $s0, 4($s0)
+move $s1, $s0
+la $s1, L12
+li $s0, 8
+move $s0, $s0
+jal $s1
+move $s0, $t0
+j L16
+L16:
+move $sp $fp
+lw $fp 0($sp)
+jr $ra
+
+L10:
+sw $fp 0($sp)
+move $fp $sp
+addi $sp $sp -20
 L19:
+lw $s0, 4($s0)
+move $s1, $s0
+lw $s0, 8($s0)
+move $s2, $s0
+lw $s0, 4($s0)
+li $s1, 1
+ble $s0, $s1, L13
+L14:
+lw $s0, 4($s0)
+move $s0, $s0
+la $s1, L12
+lw $s0, 0($s0)
+move $s1, $s0
+lw $s0, 4($s0)
+addi $s0, $s0, -1
+move $s2, $s0
+jal $s1
+move $s1, $t0
+mul $s0, $s0, $s1
+move $s0, $s0
+j L15
+L13:
+li $s0, 1
+move $s0, $s0
+j L15
+L20:
+j L18
+L18:
+move $sp $fp
+lw $fp 0($sp)
+jr $ra
+
