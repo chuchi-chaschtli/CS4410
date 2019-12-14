4a72ea4d760f4a4ab91ecfd4ad8493d65e040329
David James Reed <ddavidreed@gmail.com>
2019-12-14 09:26:40 -0500

    Most updated fact

--- tests_insn/factorial.tig.s
+++ tests_insn/factorial.tig.s
@@ -8,10 +8,11 @@ move $fp $sp
 addi $sp $sp -52
 L19:
 sw $s0, 4($s0)
+la $s1, L12
 li $s0, 8
-move $s1, $s0
-jal L12
-move $s0, $t0
+move $s2, $s0
+jalr $s1
+move $s0, $s0
 j L18
 L18:
 move $sp $fp
@@ -23,7 +24,7 @@ sw $fp 0($sp)
 move $fp $sp
 addi $sp $sp -52
 L21:
-sw $s1, 4($s0)
+sw $s2, 4($s0)
 sw $s0, 8($s0)
 li $s0, 1
 move $s1, $s0
@@ -38,15 +39,16 @@ li $s0, 1
 beq $s1, $s0, L13
 L14:
 lw $s0, 4($s0)
-move $s1, $s0
+move $s0, $s0
+la $s1, L12
 lw $s0, 0($s0)
-move $s1, $s0
+move $s2, $s0
 lw $s0, 4($s0)
 addi $s0, $s0, -1
 move $s0, $s0
-jal L12
-move $s0, $t0
-mul $s0, $s1, $s0
+jalr $s1
+move $s1, $s0
+mul $s0, $s0, $s1
 move $s0, $s0
 L15:
 j L20
